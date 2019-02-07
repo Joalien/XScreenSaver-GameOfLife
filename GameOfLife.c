378356ebfe689ec0d051967f035747e2219462af
CORNEC <josquin.cornec@insa-lyon.fr>
2019-02-07 14:24:38 +0100

    New starting motifs

--- GameOfLife.c
+++ GameOfLife.c
@@ -8,11 +8,6 @@
 
 #include "vroot.h"
 
-#define NCOLORS 2
-//#define TIME 100
-#define NOISE 10 //assert NOISE != -1
-#define ZOOM 1 //assert ZOOM != 0
-
 void my_sleep(int milliseconds){
     struct timespec ts;
     ts.tv_sec = milliseconds / 1000;
@@ -31,6 +26,128 @@ int8_t ** addPentominoR(int8_t **grid, int height, int width, int y, int x){
     return grid;
 }
 
+int8_t ** addGland(int8_t **grid, int height, int width, int y, int x){
+
+    grid[(y+2)%height][(x+0)%width] = 1;
+    grid[(y+0)%height][(x+1)%width] = 1;
+    grid[(y+2)%height][(x+1)%width] = 1;
+    grid[(y+1)%height][(x+3)%width] = 1;
+    grid[(y+2)%height][(x+4)%width] = 1;
+    grid[(y+2)%height][(x+5)%width] = 1;
+    grid[(y+2)%height][(x+6)%width] = 1;
+
+    return grid;
+}
+
+int8_t ** addLapins(int8_t **grid, int height, int width, int y, int x){
+
+    grid[(y+1)%height][(x+0)%width] = 1;
+    grid[(y+2)%height][(x+1)%width] = 1;
+    grid[(y+3)%height][(x+1)%width] = 1;
+    grid[(y+1)%height][(x+2)%width] = 1;
+    grid[(y+0)%height][(x+4)%width] = 1;
+    grid[(y+1)%height][(x+5)%width] = 1;
+    grid[(y+2)%height][(x+5)%width] = 1;
+    grid[(y+0)%height][(x+6)%width] = 1;
+    grid[(y+3)%height][(x+7)%width] = 1;
+
+
+    return grid;
+}
+
+int8_t ** addPuffer1(int8_t **grid, int height, int width, int y, int x){
+
+    grid[(y+1)%height][(x+0)%width] = 1;
+    grid[(y+3)%height][(x+0)%width] = 1;
+    grid[(y+0)%height][(x+1)%width] = 1;
+    grid[(y+1)%height][(x+2)%width] = 1;
+    grid[(y+4)%height][(x+2)%width] = 1;
+    grid[(y+3)%height][(x+3)%width] = 1;
+    grid[(y+4)%height][(x+3)%width] = 1;
+    grid[(y+5)%height][(x+3)%width] = 1;
+
+
+    return grid;
+}
+
+int8_t ** addPuffer2(int8_t **grid, int height, int width, int y, int x){
+
+    grid[(y+5)%height][(x+0)%width] = 1;
+    grid[(y+4)%height][(x+2)%width] = 1;
+    grid[(y+5)%height][(x+2)%width] = 1;
+    grid[(y+1)%height][(x+4)%width] = 1;
+    grid[(y+2)%height][(x+4)%width] = 1;
+    grid[(y+3)%height][(x+4)%width] = 1;
+    grid[(y+0)%height][(x+6)%width] = 1;
+    grid[(y+1)%height][(x+6)%width] = 1;
+    grid[(y+2)%height][(x+6)%width] = 1;
+    grid[(y+1)%height][(x+7)%width] = 1;
+
+
+
+    return grid;
+}
+
+int8_t ** addPuffer3(int8_t **grid, int height, int width, int y, int x){
+
+    grid[(y+0)%height][(x+0)%width] = 1;
+    grid[(y+1)%height][(x+0)%width] = 1;
+    grid[(y+4)%height][(x+0)%width] = 1;
+    grid[(y+0)%height][(x+1)%width] = 1;
+    grid[(y+3)%height][(x+1)%width] = 1;
+    grid[(y+0)%height][(x+2)%width] = 1;
+    grid[(y+3)%height][(x+2)%width] = 1;
+    grid[(y+4)%height][(x+2)%width] = 1;
+    grid[(y+2)%height][(x+3)%width] = 1;
+    grid[(y+0)%height][(x+4)%width] = 1;
+    grid[(y+2)%height][(x+4)%width] = 1;
+    grid[(y+3)%height][(x+4)%width] = 1;
+    grid[(y+4)%height][(x+4)%width] = 1;
+
+    return grid;
+}
+
+int8_t ** addPuffer4(int8_t **grid, int height, int width, int y, int x){
+
+    grid[y][(x+0)%width] = 1;
+    grid[y][(x+1)%width] = 1;
+    grid[y][(x+2)%width] = 1;
+    grid[y][(x+3)%width] = 1;
+    grid[y][(x+4)%width] = 1;
+    grid[y][(x+5)%width] = 1;
+    grid[y][(x+6)%width] = 1;
+    grid[y][(x+7)%width] = 1;
+
+    grid[y][(x+9)%width] = 1;
+    grid[y][(x+10)%width] = 1;
+    grid[y][(x+11)%width] = 1;
+    grid[y][(x+12)%width] = 1;
+    grid[y][(x+13)%width] = 1;
+
+    grid[y][(x+17)%width] = 1;
+    grid[y][(x+18)%width] = 1;
+    grid[y][(x+19)%width] = 1;
+
+    grid[y][(x+26)%width] = 1;
+    grid[y][(x+27)%width] = 1;
+    grid[y][(x+28)%width] = 1;
+    grid[y][(x+29)%width] = 1;
+    grid[y][(x+30)%width] = 1;
+    grid[y][(x+31)%width] = 1;
+    grid[y][(x+32)%width] = 1;
+
+    grid[y][(x+34)%width] = 1;
+    grid[y][(x+35)%width] = 1;
+    grid[y][(x+36)%width] = 1;
+    grid[y][(x+37)%width] = 1;
+    grid[y][(x+38)%width] = 1;
+
+
+
+    return grid;
+}
+
+
 int8_t getNumberOfNeighbors(int8_t ** grid, int height, int width, int x, int y){
     int8_t nombreDeVoisin=0;
     for(int j = y-1; j<=y + 1; j++){
@@ -42,43 +159,81 @@ int8_t getNumberOfNeighbors(int8_t ** grid, int height, int width, int x, int y)
     return nombreDeVoisin - grid[y%height][x%width];
 }
 
-int8_t ** updateGrid(int8_t ** grid, int height, int width){
+int8_t ** updateGrid(int8_t ** grid, int16_t ** memoryGrid, int height, int width, int noise, int memorySize){
     int x, y;
 
-    //Create a duplicated version of the grid named matrix
-    int8_t ** matrix = malloc(sizeof(*grid) * height);
+    //Create a duplicated version of the grid named copy
+    int8_t ** copy = malloc(sizeof(*grid) * height);
     for (y = 0; y < height; y++) {
-        matrix[y] = malloc(sizeof(**grid) * width);
+        copy[y] = malloc(sizeof(**grid) * width);
         for (x = 0; x < width; x++) {
-            matrix[y][x] = grid[y][x];
+            copy[y][x] = grid[y][x];
         }
     }
 
     //Update the grid
     for(y=0; y<height; y++) {
         for(x=0; x<width; x++) {
-            int8_t neighbors = getNumberOfNeighbors(matrix, height, width, x, y);
+            int8_t neighbors = getNumberOfNeighbors(copy, height, width, x, y);
             if (neighbors==3)
                 grid[y][x] = 1;
             else if((neighbors>3) || (neighbors <2))
                 grid[y][x] = 0;
+
+            //Check if one block stay at the same position
+            if(grid[y][x] == 1 && (copy[y][x] == 1 || memoryGrid[y][x]>=memorySize)){
+                memoryGrid[y][x]++;
+                if(memoryGrid[y][x]>=memorySize) grid[y][x] = 0;
+            }
+            else memoryGrid[y][x]=0;
         }
     }
 
-    //Add random pentomino
-    if(1 == (int)(rand() / (double)RAND_MAX * (NOISE - 1)))
-        addPentominoR(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+    //Add random motif
+    int randomValue = (int)(rand() / (double)RAND_MAX * (noise - 1));
+    switch (randomValue)
+    {
+        case 1:
+            addPentominoR(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                          (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 2:
+            addGland(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                     (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 3:
+            addLapins(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
                       (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 4:
+            addPuffer1(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                       (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 5:
+            addPuffer2(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                       (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 6:
+            addPuffer3(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                       (int) (rand() / (double)RAND_MAX * width));
+            break;
+        case 7:
+            addPuffer4(grid, height, width, (int) (rand() / (double)RAND_MAX * height),
+                       (int) (rand() / (double)RAND_MAX * width));
+            break;
+    }
+
 
 
     //Suppression of the duplicated version
     for (y = 0; y < height; y++) {
-        free(matrix[y]);
+        free(copy[y]);
     }
-    free(matrix);
+    free(copy);
 
     return grid;
 }
+
 
 /*TODO :
  * - Finir de trouver tous les hyperparamètres
@@ -89,13 +244,26 @@ int8_t ** updateGrid(int8_t ** grid, int height, int width){
 
 
 int main (int argc, char *argv[]) {
-    
+
+    srand((unsigned int) time(NULL)); // initialisation de rand
+
+    /** HYPERPARAMETERS **/
+    int zoom = (int) (rand() / (double)RAND_MAX * 16);
+    #define ZOOM zoom //assert ZOOM != 0
+    #define NCOLORS 2
+    #define NOISE 300//10 //assert NOISE != -1
+    #define MEMORY 600
+    #define HALF_STEP ZOOM>=5?1:0
+
+
     /*** INITIALIZATION ***/
     int screenWidth = 850;
     int screenHeight = 650;
 
     int widthOffset = (screenWidth%ZOOM)/2;
     int heightOffset = (screenHeight%ZOOM)/2;
+
+
 
     Display *dpy;
     Window window;
@@ -151,6 +319,19 @@ int main (int argc, char *argv[]) {
         }
     }
 
+    //Création dynamique de la grille
+    int16_t ** memoryGrid = malloc(sizeof(*memoryGrid) * height);
+    for (c = 0; c < height; c++) {
+        memoryGrid[c] = malloc(sizeof(**memoryGrid) * width);
+        int c2;
+        for (c2=0; c2<width; c2++){
+            memoryGrid[c][c2]=0;
+        }
+    }
+
+    //Add starting motif
+    //addPuffer4(grid, height, width, height/2, width/2);
+
 
     for(c=0; c<NCOLORS; c++) {
         //create GC for drawing in the window
@@ -174,7 +355,8 @@ int main (int argc, char *argv[]) {
             }
         }
 
-        grid = updateGrid(grid, height, width);
+        grid = updateGrid(grid, memoryGrid, height, width, NOISE, MEMORY);
+            if(HALF_STEP == 1) grid = updateGrid(grid, memoryGrid, height, width, NOISE, MEMORY);
 //        my_sleep(TIME);
 
         /* copy the buffer to the window */
